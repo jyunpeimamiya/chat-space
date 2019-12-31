3854f439eac8b5ac4ad07b86e87b3ec466a3422b
jyunpeimamiya <jyunpei0225@yahoo.com>
2019-12-31 14:49:43 +0900

    user management

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :messages,only:[:index]
-
+  resources :users,only:[:edit,:update]
   root "messages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
