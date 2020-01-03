c39214c98584fcb41887bd2444deb040bddaa2bc
jyunpeimamiya <jyunpei0225@yahoo.com>
2020-01-03 16:10:15 +0900

    group create

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  validates :name, pressence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
